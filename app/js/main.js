ca58ce422e2a84cd8e9f34ecee529426d64908b5
unknown <olysiuk01@gmail.com>
2020-11-16 22:35:39 +0200

    preloader

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 // header-menu
 new WOW().init()
 
+
+window.onload = function(){
+  
+  document.body.style.overflow = 'initial';
+  document.body.querySelector('.preloader').style.display = 'none'
+}
+
 function headerMenu(){
 
   let menu = document.querySelector('.header__top-list');
@@ -51,3 +58,18 @@ function slider(){
 }
 
 slider()
+
+function preloader(){
+var animData = {
+  wrapper: document.querySelector('#animationWindow'),
+  animType: 'svg',
+  loop: true,
+  prerender: true,
+  autoplay: true,
+  path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+};
+var anim = bodymovin.loadAnimation(animData);
+anim.setSpeed(3.4);
+}
+
+preloader()
